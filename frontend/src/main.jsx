61282f5cb57d65229372bbe9fc931ca31df274b1
yashsb-07 <yashsbansode07@gmail.com>
2026-07-12 22:50:05 +0530

    Create global design system with CSS variables, typography and utility styles

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
 import App from "./App";
-import "./index.css";
+
+import "./styles/variables.css";
+import "./styles/typography.css";
+import "./styles/global.css";
+import "./styles/utilities.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
