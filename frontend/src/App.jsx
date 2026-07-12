b4ecaa34723fc94687e689d163b0c04acdc0c96b
yashsb-07 <yashsbansode07@gmail.com>
2026-07-12 22:24:49 +0530

    Initial portfolio project setup with Vite and professional folder structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import { BrowserRouter } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <h1>Portfolio Setup Completed 🚀</h1>
+    </BrowserRouter>
+  );
+}
+
+export default App;
