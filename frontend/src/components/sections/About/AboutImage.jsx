81f4fd90a73d2c8562de782034b84f06b688b7cc
yashsb-07 <yashsbansode07@gmail.com>
2026-07-26 16:26:03 +0530

    feat: redesign about developer profile card

--- frontend/src/components/sections/About/AboutImage.jsx
+++ frontend/src/components/sections/About/AboutImage.jsx
@@ -1,3 +1,9 @@
+import {
+  LuMapPin,
+  LuGraduationCap,
+  LuCode,
+} from "react-icons/lu";
+
 import aboutData from "../../../data/aboutData";
 
 import styles from "./About.module.css";
@@ -9,22 +15,42 @@ const AboutImage = () => {
   } = aboutData;
 
   return (
-    <div className={styles.profileCard}>
+    <article className={styles.profileCard}>
+      <div
+        className={styles.profileAccent}
+        aria-hidden="true"
+      />
+
       <div className={styles.profileVisual}>
         <div
           className={styles.profileGlow}
           aria-hidden="true"
         />
 
-        <div className={styles.profileImage}>
-          <span>Your Photo</span>
+        <div
+          className={styles.profileImage}
+          aria-hidden="true"
+        >
+          <span className={styles.profileInitials}>
+            YB
+          </span>
+
+          <span className={styles.profileCodeMark}>
+            {"</>"}
+          </span>
         </div>
       </div>
 
       <div className={styles.profileContent}>
+        <span className={styles.profileEyebrow}>
+          Developer Profile
+        </span>
+
         <h3>{profile.name}</h3>
 
-        <p>{profile.role}</p>
+        <p className={styles.profileRole}>
+          {profile.role}
+        </p>
 
         <div className={styles.availabilityBadge}>
           <span
@@ -37,6 +63,69 @@ const AboutImage = () => {
           </span>
         </div>
       </div>
+
+      <div className={styles.profileDivider} />
+
+      <div className={styles.profileDetails}>
+        <div className={styles.profileDetail}>
+          <div
+            className={styles.profileDetailIcon}
+            aria-hidden="true"
+          >
+            <LuMapPin />
+          </div>
+
+          <div>
+            <span className={styles.profileDetailLabel}>
+              Location
+            </span>
+
+            <strong>
+              {profile.location}
+            </strong>
+          </div>
+        </div>
+
+        <div className={styles.profileDetail}>
+          <div
+            className={styles.profileDetailIcon}
+            aria-hidden="true"
+          >
+            <LuGraduationCap />
+          </div>
+
+          <div>
+            <span className={styles.profileDetailLabel}>
+              Education
+            </span>
+
+            <strong>
+              {profile.education}
+            </strong>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.profileFocus}>
+        <div className={styles.profileFocusHeading}>
+          <LuCode aria-hidden="true" />
+
+          <span>Current Focus</span>
+        </div>
+
+        <div className={styles.profileFocusList}>
+          {profile.focus.map((item) => (
+            <span
+              className={styles.profileFocusItem}
+              key={item}
+            >
+              {item}
+            </span>
+          ))}
+        </div>
+      </div>
+
+      <div className={styles.profileDivider} />
 
       <div
         className={styles.techStack}
@@ -57,7 +146,7 @@ const AboutImage = () => {
           );
         })}
       </div>
-    </div>
+    </article>
   );
 };
 
