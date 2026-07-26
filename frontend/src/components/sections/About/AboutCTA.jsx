676809f69698773d13018942ffa54a115d6a202e
yashsb-07 <yashsbansode07@gmail.com>
2026-07-26 17:08:21 +0530

    feat: add resume download functionality

--- frontend/src/components/sections/About/AboutCTA.jsx
+++ frontend/src/components/sections/About/AboutCTA.jsx
@@ -1,15 +1,20 @@
 import { FiDownload } from "react-icons/fi";
-import Button from "../../ui/Button/Button";
+
 import styles from "./About.module.css";
 
 const AboutCTA = () => {
   return (
     <div className={styles.cta}>
-      <Button>
-        <FiDownload />
+      <a
+        href="/Yash-Bansode-Resume.pdf"
+        download="Yash-Bansode-Resume.pdf"
+        className={styles.resumeButton}
+        aria-label="Download Yash Bansode resume as PDF"
+      >
+        <FiDownload aria-hidden="true" />
 
         <span>Download Resume</span>
-      </Button>
+      </a>
     </div>
   );
 };
